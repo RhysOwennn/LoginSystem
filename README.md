83af7aaff83b013bebe0af75b65ca8b9eec22125
RhysOwennn <69479023+RhysOwennn@users.noreply.github.com>
2023-03-22 13:46:32 +0200

    add setup, add endpoints, add connection, add schema, add models

---
+++ README.md
@@ -0,0 +1,13 @@
+
+
+
+
+
+
+Requirements:
+FastAPI https://fastapi.tiangolo.com/
+MongoDB https://www.mongodb.com/try/download/community
+
+create a virtual enviroment
+Windows: python -m venv venv
+.\venv\Scripts\activate to run the enviroment
---
+++ schemas/user.py
@@ -0,0 +1,12 @@
+def userEntity(item) -> dict:
+    return {
+        "id": str(item["_id"]),
+        "username": item["username"],
+        "email": item["email"],
+        "password": item["password"]
+    }
+
+
+
+def usersEntity(entity) -> list:
+    return [userEntity(item) for item in entity]
